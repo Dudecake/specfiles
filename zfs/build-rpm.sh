73c2c674eed8337d024e1be5dc8c3bbfca57ae86
Dudecake <coen_koomen@hotmail.com>
2021-11-20 12:01:20 +0100

    Added build for zfs-kmod

--- zfs/build-rpm.sh
+++ zfs/build-rpm.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 MACHINE="$(uname -m)"
-ZFS_VERSION="2.1.1-1"
+ZFS_VERSION="2.1.1"
 
-if [[ ! -f "${3}/${ARCH}/os/zfs-${ZFS_VERSION}.${MACHINE}.*.rpm" ]]; then
-  curl -L --remote-name-all http://download.zfsonlinux.org/fedora/34/SRPMS/zfs{,-dkms}-${ZFS_VERSION}.fc34.src.rpm
-  exec mock -r "${1}-${MACHINE}" rebuild ./*.src.rpm --resultdir "${2}"
+if [[ ! -f "${3}/${ARCH}/os/zfs-${ZFS_VERSION}-1.${MACHINE}.*.rpm" ]]; then
+  curl -L https://github.com/openzfs/zfs/releases/download/zfs-${ZFS_VERSION}/zfs-${ZFS_VERSION}.tar.gz | tar -xzf -
+  pushd zfs-${ZFS_VERSION} > /dev/null
+  ./configure
+  make -j1 srpm
+  popd > /dev/null
+  exec mock -r "${1}-${MACHINE}" rebuild ./zfs-${ZFS_VERSION}/zfs*.src.rpm --resultdir "${2}"
 fi
