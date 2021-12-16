c4177a3f43eaf40bba81d028c920bbc9122eb612
Dudecake <coen_koomen@hotmail.com>
2021-12-16 13:30:27 +0000

    Updated kubernetes

--- zfs/build-rpm.sh
+++ zfs/build-rpm.sh
@@ -6,7 +6,7 @@ ZFS_VERSION="2.1.2"
 if [[ ! -f "${2}/${ARCH}/os/zfs-${ZFS_VERSION}-1.${MACHINE}.*.rpm" ]]; then
   curl -L https://github.com/openzfs/zfs/releases/download/zfs-${ZFS_VERSION}/zfs-${ZFS_VERSION}.tar.gz | tar -xzf -
   pushd zfs-${ZFS_VERSION} > /dev/null
-  PYTHON=/usr/bin/python3.9 ./configure
+  ./configure
   make -j1 srpm
   popd > /dev/null
   rpmbuild --rebuild ./zfs-${ZFS_VERSION}/*.src.rpm -D "_rpmdir ${1}" -D "_srcrpmdir ${1}"
