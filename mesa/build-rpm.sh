18da3b061ae5e9132a3ff6ef515627bcf27f011e
Dudecake <coen_koomen@hotmail.com>
2022-04-16 10:37:06 +0200

    Fixed mesa build

--- mesa/build-rpm.sh
+++ mesa/build-rpm.sh
@@ -11,10 +11,10 @@ else
   popd > /dev/null
 fi
 readarray -t COMMIT_DATA <<< "$(curl -sS https://gitlab.freedesktop.org/api/v4/projects/176/repository/commits | jq '.[0] | .short_id, .created_at')"
-HASH="${COMMIT_DATA[0]})"
+HASH="${COMMIT_DATA[0]:1:7}"
 DATE="$(echo ${COMMIT_DATA[1]:1:10} | sed 's/-//g')"
 VER="$(grep -oP '(?<=ver )\d+\.\d\.\d' ./${PKG}/${PKG}.spec)"
-sed -i "s/${VER}/22.2.0/; s/%autorelease/10.${DATE}.git${HASH:0:7}%{?dist}/; s/https:\/\/mesa.freedesktop.org\/archive\/%{name}-%{ver}.tar.xz/https:\/\/gitlab.freedesktop.org\/mesa\/mesa\/-\/archive\/${HASH}\/mesa-${HASH}.tar.gz/; s/%{name}-%{ver}/%{name}-${HASH}/; /00-mesa-defaults.conf/a %{_datadir}/drirc.d/00-radv-defaults.conf" ./${PKG}/${PKG}.spec
+sed -i "s/${VER}/22.2.0/; s/%autorelease/10.${DATE}.git${HASH}%{?dist}/; s/https:\/\/mesa.freedesktop.org\/archive\/%{name}-%{ver}.tar.xz/https:\/\/gitlab.freedesktop.org\/mesa\/mesa\/-\/archive\/${HASH}\/mesa-${HASH}.tar.gz/; s/%{name}-%{ver}/%{name}-${HASH}/; /00-mesa-defaults.conf/a %{_datadir}/drirc.d/00-radv-defaults.conf" ./${PKG}/${PKG}.spec
 
 spectool -g ./${PKG}/${PKG}.spec -C ${PKG}
 rpmbuild -bs ./${PKG}/${PKG}.spec -D "_srcrpmdir ${PWD}" -D "_sourcedir ${PWD}/${PKG}"
