7c9b960b9f841f3330c46defb8c03916f37e2c62
Dudecake <coen_koomen@hotmail.com>
2022-04-21 22:15:33 +0200

    Added packages from CentOS GlusterFS SIG

---
+++ glusterfs/build-rpm.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+PKG=glusterfs
+set -e
+if [[ ! -d ${PKG}/.git ]]; then
+  git clone https://git.centos.org/rpms/${PKG}.git --branch c8s-sig-storage-gluster-10  --single-branch
+else
+  pushd ./${PKG} > /dev/null
+  git pull
+  popd > /dev/null
+fi
+spectool -g ./${PKG}/SPECS/${PKG}.spec -C ${PKG}/SOURCES
+rpmbuild -bs ./${PKG}/SPECS/${PKG}.spec -D "_srcrpmdir ${PWD}" -D "_sourcedir ${PWD}/${PKG}/SOURCES"
+RPM_FILE=$(ls -1 ./*.src.rpm | head -n1)
+if [[ ! -f "${2}/source/tree/${RPM_FILE}" ]]; then
+  dnf builddep -y ./*.src.rpm --enablerepo powertools
+  rpmbuild --rebuild ./*.src.rpm -D "_rpmdir ${1}" -D "_srcrpmdir ${1}"
+  mv ./*.src.rpm "${1}"
+else
+  echo "No rebuild neccesary for package $(basename $PWD)" >&2
+fi
