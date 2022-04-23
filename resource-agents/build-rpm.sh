5a5a075b96de0a5b1a59c4608cfe68ef0d8e48e1
Dudecake <coen_koomen@hotmail.com>
2022-04-23 13:07:45 +0200

    Changed generation of src rpm

--- resource-agents/build-rpm.sh
+++ resource-agents/build-rpm.sh
@@ -2,32 +2,7 @@
 
 PKG=resource-agents
 set -e
-if [[ ! -d ${PKG}/.git ]]; then
-  git clone https://git.centos.org/rpms/${PKG}.git --branch c8s  --single-branch
-else
-  pushd ./${PKG} > /dev/null
-  git pull
-  popd > /dev/null
-fi
-HASH=$(spectool -l ./${PKG}/SPECS/${PKG}.spec | grep -Po '[0-9a-f]{8}')
-if [[ ! -d ${PKG}-upstream/.git ]]; then
-  git clone https://github.com/ClusterLabs/resource-agents.git --branch main --single-branch ${PKG}-upstream
-  pushd ./${PKG}-upstream > /dev/null
-  git checkout ${HASH}
-else
-  pushd ./${PKG}-upstream > /dev/null
-  git pull
-  git checkout ${HASH}
-fi
-TARFILE=../${PKG}/SOURCES/ClusterLabs-resource-agents-${HASH}.tar.gz
-TAG=$(git log --pretty="format:%h" -n 1)
-distdir="ClusterLabs-resource-agents-${TAG}"
-TARFILE="${distdir}.tar.gz"
-rm -rf $TARFILE $distdir
-git archive --prefix=$distdir/ HEAD | gzip > $TARFILE
-popd > /dev/null
-spectool -g ./${PKG}/SPECS/${PKG}.spec -C ${PKG}/SOURCES
-rpmbuild -bs ./${PKG}/SPECS/${PKG}.spec -D "_srcrpmdir ${PWD}" -D "_sourcedir ${PWD}/${PKG}/SOURCES"
+dnf download --source resource-agents --repofrompath ha-sourcer,http://vault.centos.org/centos/8-stream/HighAvailability/Source/
 RPM_FILE=$(ls -1 ./*.src.rpm | head -n1)
 if [[ ! -f "${2}/source/tree/${RPM_FILE}" ]]; then
   dnf builddep -y ./*.src.rpm --enablerepo powertools
