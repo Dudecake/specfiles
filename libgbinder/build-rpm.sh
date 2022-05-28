0fb178a1fb595529eeb1778c79ec30c5b496361e
Dudecake <coen_koomen@hotmail.com>
2022-05-28 14:37:49 +0200

    Swapped directories for waydroid packages

--- libgbinder/build-rpm.sh
+++ libgbinder/build-rpm.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 ../build-rpm.sh ${1} ${2}
-RPMS="${1}/libgbinder*.rpm"
+RPMS="${2}/libgbinder*.rpm"
 if [[ ! -f $RPMS ]]; then
-  RPMS=${2}/$(uname -m)/os/libgbinder*.rpm
+  RPMS=${1}/$(uname -m)/os/libgbinder*.rpm
 fi
 dnf install -y ${RPMS}
--- python3-gbinder/build-rpm.sh
+++ python3-gbinder/build-rpm.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 ../build-rpm.sh ${1} ${2}
-RPMS="${1}/python3-gbinder*.rpm"
+RPMS="${2}/python3-gbinder*.rpm"
 if [[ ! -f $RPMS ]]; then
-  RPMS=${2}/$(uname -m)/os/python3-gbinder*.rpm
+  RPMS=${1}/$(uname -m)/os/python3-gbinder*.rpm
 fi
 dnf install -y ${RPMS}
