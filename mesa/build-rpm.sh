0fb3b05a09de0ceab03488f779e0a19b06b0e670
Dudecake <coen_koomen@hotmail.com>
2022-08-06 12:56:33 +0200

    Changed mesa version to same as rawhide

--- mesa/build-rpm.sh
+++ mesa/build-rpm.sh
@@ -10,11 +10,11 @@ else
   git pull
   popd > /dev/null
 fi
-readarray -t COMMIT_DATA <<< "$(curl -sS https://gitlab.freedesktop.org/api/v4/projects/176/repository/commits | jq '.[0] | .short_id, .created_at')"
-HASH="${COMMIT_DATA[0]:1:7}"
-DATE="$(echo ${COMMIT_DATA[1]:1:10} | sed 's/-//g')"
-VER="$(grep -oP '(?<=ver )\d+\.\d\.\d' ./${PKG}/${PKG}.spec)"
-sed -i "s/${VER}/22.2.0/; s/%autorelease/10.${DATE}.git${HASH}%{?dist}/; s/https:\/\/archive.mesa3d.org\/mesa-%{ver}.tar.xz/https:\/\/gitlab.freedesktop.org\/mesa\/mesa\/-\/archive\/${HASH}\/mesa-${HASH}.tar.gz/; s/%{name}-%{ver}/%{name}-${HASH}/" ./${PKG}/${PKG}.spec
+# readarray -t COMMIT_DATA <<< "$(curl -sS https://gitlab.freedesktop.org/api/v4/projects/176/repository/commits | jq '.[0] | .short_id, .created_at')"
+# HASH="${COMMIT_DATA[0]:1:7}"
+# DATE="$(echo ${COMMIT_DATA[1]:1:10} | sed 's/-//g')"
+# VER="$(grep -oP '(?<=ver )\d+\.\d\.\d' ./${PKG}/${PKG}.spec)"
+# sed -i "s/${VER}/22.2.0/; s/%autorelease/10.${DATE}.git${HASH}%{?dist}/; s/https:\/\/archive.mesa3d.org\/mesa-%{ver}.tar.xz/https:\/\/gitlab.freedesktop.org\/mesa\/mesa\/-\/archive\/${HASH}\/mesa-${HASH}.tar.gz/; s/%{name}-%{ver}/%{name}-${HASH}/" ./${PKG}/${PKG}.spec
 
 spectool -g ./${PKG}/${PKG}.spec -C ${PKG}
 rpmbuild -bs ./${PKG}/${PKG}.spec -D "_srcrpmdir ${PWD}" -D "_sourcedir ${PWD}/${PKG}"
