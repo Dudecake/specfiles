e7ddc6f7c444a1545f6d43f250471ff2caaf2956
Dudecake <coen_koomen@hotmail.com>
2022-11-04 21:22:22 +0100

    Fixed `virglrenderer` build

--- virglrenderer/build-rpm.sh
+++ virglrenderer/build-rpm.sh
@@ -12,7 +12,7 @@ else
   popd > /dev/null
 fi
 HASH=$(grep -oP '(?<=gitversion ).+' ./${PKG}/${PKG}.spec)
-sed -i "s/virglrenderer-%{gitdate}.tar.xz/https:\/\/gitlab.freedesktop.org\/${GROUP}\/${PKG}\/-\/archive\/${HASH}\/${PKG}-${HASH}.tar.gz/; s/%meson/%meson -Dvenus-experimental=true/" ./${PKG}/${PKG}.spec
+sed -i "s/virglrenderer-%{gitdate}.tar.xz/https:\/\/gitlab.freedesktop.org\/${GROUP}\/${PKG}\/-\/archive\/${HASH}\/${PKG}-${HASH}.tar.gz/; s/%{name}-%{gitdate}/%{name}-%{gitversion}/; s/%meson /%meson -Dvenus-experimental=true /" ./${PKG}/${PKG}.spec
 set +e
 ln -s ./${PKG}/* ./
 
