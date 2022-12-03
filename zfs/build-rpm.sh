8a0f1ae6d25f4038286c63e53877e946ad4490df
Dudecake <coen_koomen@hotmail.com>
2022-12-03 23:35:40 +0100

    Fixed `zfs` build for centos 9-stream

--- zfs/build-rpm.sh
+++ zfs/build-rpm.sh
@@ -7,7 +7,7 @@ ZFS_VERSION="2.1.7"
 if [[ ! -f "${2}/${MACHINE}/os/kmod-zfs-${KERNEL_VERSION}-${ZFS_VERSION}-1*.${MACHINE}.rpm" ]]; then
   curl -sSL https://github.com/openzfs/zfs/releases/download/zfs-${ZFS_VERSION}/zfs-${ZFS_VERSION}.tar.gz | tar -xzf -
   pushd zfs-${ZFS_VERSION} > /dev/null
-  dnf install -y --skip-broken epel-release gcc make autoconf automake libtool rpm-build libtirpc-devel libblkid-devel libuuid-devel libudev-devel openssl-devel zlib-devel libaio-devel libattr-devel elfutils-libelf-devel kernel-devel python3 python3-devel python3-setuptools python3-cffi libffi-devel git ncompress libcurl-devel
+  dnf install -y --skip-broken epel-release gcc make autoconf automake libtool rpm-build libtirpc-devel libblkid-devel libuuid-devel libudev-devel openssl-devel zlib-devel libaio-devel libattr-devel elfutils-libelf-devel kernel-devel python3 python3-devel python3-setuptools python3-cffi libffi-devel git ncompress libcurl-devel --enablerepo crb
   dnf install -y --skip-broken --enablerepo=epel --enablerepo=powertools python3-packaging dkms
   ./configure
   make -j1 srpm
