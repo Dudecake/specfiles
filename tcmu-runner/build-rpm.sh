f57b4cf0527a7a5adc807407bcbe2c5a4140d3c8
Dudecake <coen_koomen@hotmail.com>
2022-12-13 21:39:01 +0100

    Added `tcmu-runner`

---
+++ tcmu-runner/build-rpm.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+PKG=tcmu-runner
+set -e
+if [[ ! -d ${PKG}/.git ]]; then
+  git clone --branch c9s-sig-storage-gluster-9 --depth=1 https://git.centos.org/rpms/tcmu-runner.git
+else
+  pushd ./${PKG} > /dev/null
+  git restore ${PKG}.spec
+  git pull
+  popd > /dev/null
+fi
+
+ln -s ./${PKG}/*/* ./
+
+exec ../build-rpm.sh "$@"
