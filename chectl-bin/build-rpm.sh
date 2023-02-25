917334d5f4187ae37071f9b13df86dd42188a7ba
Dudecake <coen_koomen@hotmail.com>
2023-02-25 11:24:32 +0100

    Removed duplicate rpm build code

--- chectl-bin/build-rpm.sh
+++ chectl-bin/build-rpm.sh
@@ -5,14 +5,4 @@ BASEARCH="${MACHINE}"
 [[ "${MACHINE}" = "x86_64" ]] && BASEARCH="x64"
 [[ "${MACHINE}" = "aarch64" ]] && BASEARCH="arm64"
 BASEARCH=${BASEARCH} envsubst < ./chectl-bin.spec.in > ./chectl-bin.spec
-spectool -g ./chectl-bin.spec
-rpmbuild -bs ./chectl-bin.spec -D "_srcrpmdir ${PWD}" -D "_sourcedir ${PWD}"
-RPM_FILE=$(ls -1 ./*.src.rpm | head -n1)
-if [[ ! -f "${2}/source/tree/${RPM_FILE}" ]]; then
-  dnf builddep -y ./*.src.rpm
-  rpmbuild --rebuild ./*.src.rpm -D "_rpmdir ${1}" -D "_srcrpmdir ${1}"
-  mv ./*.src.rpm "${1}"
-else
-  rm ./*.src.rpm
-  echo "No rebuild neccesary for package $(basename $PWD)" >&2
-fi
+exec ../build-rpm.sh "$@"
--- helm-bin/build-rpm.sh
+++ helm-bin/build-rpm.sh
@@ -7,14 +7,4 @@ BASEARCH="${MACHINE}"
 
 set -e
 BASEARCH="${BASEARCH}" envsubst <./helm-bin.spec.in > ./helm-bin.spec
-spectool -g ./helm-bin.spec
-rpmbuild -bs ./helm-bin.spec -D "_srcrpmdir ${PWD}" -D "_sourcedir ${PWD}"
-RPM_FILE=$(ls -1 ./*.src.rpm | head -n1)
-if [[ ! -f "${2}/source/tree/${RPM_FILE}" ]]; then
-  dnf builddep -y ./*.src.rpm
-  rpmbuild --rebuild ./*.src.rpm -D "_rpmdir ${1}" -D "_srcrpmdir ${1}"
-  mv ./*.src.rpm "${1}"
-else
-  rm ./*.src.rpm
-  echo "No rebuild neccesary for package $(basename $PWD)" >&2
-fi
+exec ../build-rpm.sh "$@"
