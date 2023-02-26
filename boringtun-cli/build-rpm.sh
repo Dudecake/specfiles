cac9333d3353dae0af072264dec773433b7a5a4c
Dudecake <coen_koomen@hotmail.com>
2023-02-26 09:39:31 +0100

    Renamed `boringtun` to `boringtun-cli`

--- boringtun-cli/build-rpm.sh
+++ boringtun-cli/build-rpm.sh
@@ -3,9 +3,9 @@
 VERSION="0.5.2"
 
 curl -sSL https://github.com/cloudflare/boringtun/archive/refs/tags/boringtun-cli-${VERSION}.tar.gz | tar -xzf -
-pushd boringtun-${VERSION} > /dev/null
+pushd boringtun-cli-${VERSION} > /dev/null
 cargo vendor
 popd > /dev/null
-tar -cf $(rpm -E '%{_sourcedir}')/boringtun-${VERSION}.tar.gz boringtun-${VERSION}
-VERSION="${VERSION}" envsubst <./boringtun.spec.in > ./boringtun.spec
+tar -cf $(rpm -E '%{_sourcedir}')/boringtun-cli-${VERSION}.tar.gz boringtun-cli-${VERSION}
+VERSION="${VERSION}" envsubst <./boringtun-cli.spec.in > ./boringtun-cli.spec
 exec ../build-rpm.sh "$@"
