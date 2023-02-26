07cdf40c3470b7d652fe1ec61f0bef5e3f9902e5
Dudecake <coen_koomen@hotmail.com>
2023-02-26 10:02:47 +0100

    Added copying local files to `_srcrpmdir`

--- build-rpm.sh
+++ build-rpm.sh
@@ -2,6 +2,7 @@
 
 set -e
 spectool -gR ./*.spec
+pectool ./*.spec | grep -Po '(?<=: )(?!http).+' | xargs -I{} cp {} $(rpm -E '%{_sourcedir}')
 rpmbuild -bs ./*.spec -D "_srcrpmdir ${PWD}"
 RPM_FILE=$(ls -1 ./*.src.rpm | head -n1)
 if [[ ! -f "${2}/source/tree/${RPM_FILE}" ]]; then
