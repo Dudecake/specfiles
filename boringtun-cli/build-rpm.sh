9135b40075668277aa0015b872376575d367ab19
Dudecake <ckoomen@ckoomen.eu>
2023-04-01 23:23:24 +0200

    Fixed `boringtun-cli` build

--- boringtun-cli/build-rpm.sh
+++ boringtun-cli/build-rpm.sh
@@ -5,8 +5,8 @@ VERSION="0.5.2"
 curl -sSL https://github.com/cloudflare/boringtun/archive/refs/tags/boringtun-cli-${VERSION}.tar.gz | tar -xzf -
 mv boringtun-boringtun-cli-${VERSION} boringtun-cli-${VERSION}
 pushd boringtun-cli-${VERSION} > /dev/null
-cargo vendor
+CARGO_HOME=/usr/share/cargo cargo fetch
 popd > /dev/null
-tar -cf $(rpm -E '%{_sourcedir}')/boringtun-cli-${VERSION}.tar.gz boringtun-cli-${VERSION}
+tar -cf boringtun-cli-${VERSION}.tar.gz boringtun-cli-${VERSION}
 sed "s/\${VERSION}/${VERSION}/" ./boringtun-cli.spec.in > ./boringtun-cli.spec
 exec ../build-rpm.sh "$@"
