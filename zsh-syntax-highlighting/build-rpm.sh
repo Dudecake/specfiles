7e6d5d6b7bb4f869f29d2781ce224f58b06c99da
Dudecake <ckoomen@ckoomen.eu>
2023-05-08 20:54:49 +0200

    Fixed `zsh-syntax-highlighting` build

--- zsh-syntax-highlighting/build-rpm.sh
+++ zsh-syntax-highlighting/build-rpm.sh
@@ -5,6 +5,6 @@ BRANCH=rawhide
 set -e
 ../fetch-fedora-pkg.sh ${PKG} ${BRANCH}
 
-sed -i 's:{_prefix}:{_prefix} DOC_DIR=%{_docdir}/%{name}:' ./${PKG}.spec
-exit
+sed -i 's:{_prefix}:{_prefix} DOC_DIR=%{buildroot}%{_docdir}/%{name} INSTALL="%{__install} -c":' ./${PKG}.spec
+
 exec ../build-rpm.sh "$@"
