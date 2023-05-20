94284bd4ef2f6af55e8ed8e12357a5918db86df5
Dudecake <ckoomen@ckoomen.eu>
2023-05-20 10:43:47 +0200

    Added `maven-wrapper`

---
+++ maven-wrapper/maven-wrapper.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [[ ! -f pom.xml ]]; then
+  echo "No 'pom.xml' file present" >&2
+  exit 1
+fi
+
+if [[ -f mvnw ]]; then
+  echo "Wrapper already installed" >&2
+  exit 0
+fi
+
+MAVEN_VERSION=${MAVEN_VERSION:-3.9.1}
+MAVEN_URL=https://repo.maven.apache.org/maven2/org/apache/maven/apache-maven/${MAVEN_VERSION}/apache-maven-${MAVEN_VERSION}-bin.zip
+WRAPPER_VERSION=${WRAPPER_VERSION:-3.2.0}
+WRAPPER_URL=https://repo.maven.apache.org/maven2/org/apache/maven/wrapper/maven-wrapper/${WRAPPER_VERSION}/maven-wrapper-${WRAPPER_VERSION}.jar
+
+curl -sSL --remote-name-all https://github.com/apache/maven-wrapper/raw/maven-wrapper-${WRAPPER_VERSION}/maven-wrapper-distribution/src/resources/mvnw{,.cmd}
+chmod +x mvnw
+mkdir -p .mvn/wrapper
+cat << EOF > .mvn/wrapper/maven-wrapper.properties
+distributionUrl=${MAVEN_URL}
+wrapperUrl=${WRAPPER_URL}
+EOF
+curl -L ${WRAPPER_URL} -o .mvn/wrapper/maven-wrapper.jar
---
+++ maven-wrapper/maven-wrapper.spec
@@ -0,0 +1,19 @@
+Name:           maven-wrapper
+Version:        0.0.1
+Release:        1%{?dist}
+Summary:        Maven wrapper installer script
+
+License:        EUPL-1.2
+URL:            https://repo.ckoomen.eu/
+Source0:        maven-wrapper.sh
+
+BuildArch:      noarch
+
+%description
+%{summary}
+
+%install
+install -Dm755 maven-wrapper.sh %{buildroot}%{_bindir}/maven-wrapper
+
+%files
+%{_bindir}/maven-wrapper
