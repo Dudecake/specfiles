67b7d8a08dd98f82da4e801b82c7c4fd2b859c7d
Dudecake <ckoomen@ckoomen.eu>
2023-08-26 15:35:29 +0200

    Added configs for split `kernel-ckoomen` build

--- kernel-ckoomen/build-rpm.sh
+++ kernel-ckoomen/build-rpm.sh
@@ -2,10 +2,13 @@
 
 set -e
 ARCH="$(rpm -E '%_arch')"
+srcdir="${1}/kernel-source"
+specfile="${srcdir}/kernel-ckoomen.spec"
 
 [[ -d kernel-source ]] || git clone https://github.com/openSUSE/kernel-source -b stable --depth=1
 
 pushd kernel-source
+grep -q 'SPLIT_BASE' rpm/config.sh || echo 'SPLIT_BASE=Yes' >> rpm/config.sh
 source rpm/config.sh
 if [[ ! -f ./config/${ARCH}/ckoomen ]]; then
   cp ../ckoomen ./config/${ARCH}/
@@ -19,14 +22,16 @@ EOF
 fi
 suffix=$(sed -rn 's/^Source0:.*\.(tar\.[a-z0-9]*)$/\1/p' rpm/kernel-source.spec.in)
 [[ -f "linux-${SRCVERSION}.${suffix}" ]] || curl -L --remote-name-all https://cdn.kernel.org/pub/linux/kernel/v${SRCVERSION:0:1}.x/linux-${SRCVERSION}.{${suffix},${suffix:0:3}.sign}
-./scripts/tar-up.sh -a ${ARCH} -f ckoomen
-touch ./kernel-source/TOLERATE-UNKNOWN-NEW-CONFIG-OPTIONS
+./scripts/tar-up.sh -d "${srcdir}" -a ${ARCH} -f ckoomen
+touch "${srcdir}/TOLERATE-UNKNOWN-NEW-CONFIG-OPTIONS"
+[[ -z "${SBOOT_BASE}" ]] || cp ${SBOOT_BASE}.* "${srcdir}"
 popd > /dev/null
-rpmbuild -bs ./kernel-source/kernel-source/kernel-ckoomen.spec -D "_srcrpmdir ${PWD}" -D "_sourcedir ${PWD}/kernel-source/kernel-source"
+rpmbuild -bs "${specfile}" -D "_srcrpmdir ${PWD}" -D "_sourcedir ${srcdir}"
 RPM_FILE="$(ls -1 kernel-ckoomen-*.nosrc.rpm)"
 if [[ ! -z "${FORCE_REBUILD}" || ! -f "${2}/source/tree/${RPM_FILE}" ]]; then
   dnf builddep -y ./*.nosrc.rpm
-  exec rpmbuild -bb ./kernel-source/kernel-source/kernel-ckoomen.spec -D "_rpmdir ${1}" -D "_srcrpmdir ${1}" -D "_sourcedir ${PWD}/kernel-source/kernel-source"
+  rpmbuild -bb "${specfile}" -D "_rpmdir ${1}" -D "_srcrpmdir ${1}" -D "_sourcedir ${srcdir}"
 else
   echo "No rebuild neccesary for package $(basename $PWD)" >&2
 fi
+[[ -z "${SBOOT_BASE}" ]] || cp ${SBOOT_BASE}.* "${srcdir}/${SBOOT_BASE##*/}"
