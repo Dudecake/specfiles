17db561583b717673f981e3e3a48b754b17426aa
Dudecake <ckoomen@ckoomen.eu>
2023-08-30 22:14:37 +0200

    Fixed handling of `nosrc.rpm`

--- kernel-ckoomen/build-rpm.sh
+++ kernel-ckoomen/build-rpm.sh
@@ -8,7 +8,6 @@ specfile="${srcdir}/kernel-ckoomen.spec"
 [[ -d kernel-source ]] || git clone https://github.com/openSUSE/kernel-source -b stable --depth=1
 
 pushd kernel-source
-grep -q 'SPLIT_BASE' rpm/config.sh || echo 'SPLIT_BASE=Yes' >> rpm/config.sh
 source rpm/config.sh
 if [[ ! -f ./config/${ARCH}/ckoomen ]]; then
   cp ../ckoomen ./config/${ARCH}/
@@ -24,14 +23,19 @@ suffix=$(sed -rn 's/^Source0:.*\.(tar\.[a-z0-9]*)$/\1/p' rpm/kernel-source.spec.
 [[ -f "linux-${SRCVERSION}.${suffix}" ]] || curl -L --remote-name-all https://cdn.kernel.org/pub/linux/kernel/v${SRCVERSION:0:1}.x/linux-${SRCVERSION}.{${suffix},${suffix:0:3}.sign}
 ./scripts/tar-up.sh -d "${srcdir}" -a ${ARCH} -f ckoomen
 touch "${srcdir}/TOLERATE-UNKNOWN-NEW-CONFIG-OPTIONS"
-[[ -z "${SBOOT_BASE}" ]] || cp ${SBOOT_BASE}.* "${srcdir}"
+if [[ ! -z "${SBOOT_BASE}" ]]; then
+  cp ${SBOOT_BASE}.* "${srcdir}"
+  cp ${SBOOT_BASE}.crt "${srcdir}/_projectcert.crt"
+fi
 popd > /dev/null
 rpmbuild -bs "${specfile}" -D "_srcrpmdir ${PWD}" -D "_sourcedir ${srcdir}"
 RPM_FILE="$(ls -1 kernel-ckoomen-*.nosrc.rpm)"
 if [[ ! -z "${FORCE_REBUILD}" || ! -f "${2}/source/tree/${RPM_FILE}" ]]; then
   dnf builddep -y ./*.nosrc.rpm
   dnf install -y /usr/bin/openssl
-  rpmbuild -bb "${specfile}" -D "_rpmdir ${1}" -D "_srcrpmdir ${1}" -D "_sourcedir ${srcdir}"
+  BRP_PESIGN_COMPRESS_MODULE=${COMPRESS_MODULES} rpmbuild -bb "${specfile}" -D "_rpmdir ${1}" -D "_srcrpmdir ${1}" -D "_sourcedir ${srcdir}"
+  mkdir "${1}/noarch"
+  mv ./kernel-ckoomen-*.nosrc.rpm "${1}/noarch"
 else
   echo "No rebuild neccesary for package $(basename $PWD)" >&2
 fi
