4703acc9f7d4f9db03931c257f11bd0e56fd5c26
Dudecake <ckoomen@ckoomen.eu>
2023-08-30 21:43:12 +0200

    Updated `ipxe`

--- ipxe/build-rpm.sh
+++ ipxe/build-rpm.sh
@@ -2,10 +2,12 @@
 
 set -e
 dnf download --repofrompath ipxe,https://download.opensuse.org/tumbleweed/repo/src-oss/ --repo ipxe --source ipxe
-bsdtar -xf ipxe-*.src.rpm
-sed "s/__release__/$(rpm -q ipxe-*.src.rpm --qf '%{release}'/)" ./ipxe.spec.patch.in > ./ipxe.spec.patch
+srcrpm=ipxe-*.src.rpm
+bsdtar -xf ${srcrpm}
+release="$(rpm -q ${srcrpm} --qf '%{release}')"
+sed "s/__release__/${release}/; s/__new_release__/$(( ${release} + 1 ))/" ./ipxe.spec.patch.in > ./ipxe.spec.patch
 patch < ./ipxe.spec.patch
-sed -i 's/%{gcc_version}/12/g' ./ipxe.spec
+sed -i "s/%{gcc_version}/$(rpm -q gcc --qf '%{version}')/g" ./ipxe.spec
 if [[ -z "$(rpm -E '%{?suse_version}')" ]]; then
   for arch in aarch64 x86_64; do
     for item in $(find /usr/bin -maxdepth 1 -type f -name ${arch}\*-linux-\*); do
@@ -13,4 +15,5 @@ if [[ -z "$(rpm -E '%{?suse_version}')" ]]; then
     done
   done
 fi
+rm ${srcrpm}
 exec ../build-rpm.sh "$@"
