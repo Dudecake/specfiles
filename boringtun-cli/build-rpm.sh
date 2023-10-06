1a86992af5f048b61e88306a5e7cd3b44af82f03
Dudecake <ckoomen@ckoomen.eu>
2023-10-06 22:07:14 +0200

    Updated `boringtun-cli`

--- boringtun-cli/build-rpm.sh
+++ boringtun-cli/build-rpm.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+set -e
+
 VERSION="0.5.2"
 
-curl -sSL https://github.com/cloudflare/boringtun/archive/refs/tags/boringtun-cli-${VERSION}.tar.gz | tar -xzf -
-mv boringtun-boringtun-cli-${VERSION} boringtun-cli-${VERSION}
-pushd boringtun-cli-${VERSION} > /dev/null
-CARGO_HOME=/usr/share/cargo cargo fetch
+tarball="$(rpm -E %{_sourcedir}/boringtun-cli-${VERSION}.tar.gz)"
+curl -L https://github.com/cloudflare/boringtun/archive/refs/tags/boringtun-cli-${VERSION}.tar.gz -o "${tarball}"
+bsdtar -xf "${tarball}"
+pushd boringtun-boringtun-cli-${VERSION} > /dev/null
+dnf install -y cargo
+cargo vendor
+bsdtar -cf ../vendor.tar.zst vendor
 popd > /dev/null
-tar -cf boringtun-cli-${VERSION}.tar.gz boringtun-cli-${VERSION}
 sed "s/\${VERSION}/${VERSION}/" ./boringtun-cli.spec.in > ./boringtun-cli.spec
 exec ../build-rpm.sh "$@"
