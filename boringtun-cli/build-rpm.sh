cd2eab3d71d453e3ae6e316163afe1929e2a64fd
Dudecake <ckoomen@ckoomen.eu>
2023-10-14 14:18:50 +0200

    Fixed `boringtun-cli` build

--- boringtun-cli/build-rpm.sh
+++ boringtun-cli/build-rpm.sh
@@ -6,7 +6,7 @@ VERSION="0.5.2"
 
 sed "s/\${VERSION}/${VERSION}/" ./boringtun-cli.spec.in > ./boringtun-cli.spec
 rpm_file=$(python3 -c "import specfile; print(specfile.Specfile(\"boringtun-cli.spec\").expand(\"%name-%version-%release.src.rpm\"))")
-if [[ ! -f ! -f "${2}/source/tree/${RPM_FILE}" ]]; then
+if [[ ! -f "${2}/source/tree/${RPM_FILE}" ]]; then
   tarball="$(rpm -E %{_sourcedir}/boringtun-cli-${VERSION}.tar.gz)"
   curl -L https://github.com/cloudflare/boringtun/archive/refs/tags/boringtun-cli-${VERSION}.tar.gz -o "${tarball}"
   bsdtar -xf "${tarball}"
