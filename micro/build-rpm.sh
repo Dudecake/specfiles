2bb59323fa66b4751b16f4f3bc1dd407b77b3440
Dudecake <ckoomen@ckoomen.eu>
2023-10-15 13:27:34 +0200

    Added `.skip` for `micro`

--- micro/build-rpm.sh
+++ micro/build-rpm.sh
@@ -6,6 +6,6 @@ summary="A modern and intuitive terminal-based text editor"
 url="https://github.com/zyedidia/micro"
 license="MIT and ASL 2.0"
 
-version=${version} release=${release} summary="${summary}" url=${url} license=${license} ../build-go-rpm.sh > nwg-bar.spec
+version=${version} release=${release} summary="${summary}" url=${url} license=${license} ../build-go-rpm.sh > micro.spec
 
 exec ../build-rpm.sh "$@"
