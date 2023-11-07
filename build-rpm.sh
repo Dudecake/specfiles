1fc36ba45250db125da861711fe0e2258bda15f7
Dudecake <ckoomen@ckoomen.eu>
2023-11-07 22:53:10 +0100

    Fixed incorrect wildcard use

--- build-rpm.sh
+++ build-rpm.sh
@@ -4,9 +4,9 @@ set -ex
 SPEC_FILE="$(ls -1 ./*.spec | head -n1)"
 RPM_FILE=$(python3 -c "import specfile; print(specfile.Specfile(\"${SPEC_FILE}\").expand(\"%name-%version-%release.src.rpm\"))")
 if [[ ! -z "${FORCE_REBUILD}" || ! -f "${2}/source/tree/${RPM_FILE}" ]]; then
-  spectool -g -R ./*.spec
-  spectool ./*.spec | grep -Po '(?<=: )(?!http).+' | xargs -I{} cp {} $(rpm -E '%{_sourcedir}')
-  rpmbuild -bs ./*.spec -D "_srcrpmdir ${PWD}"
+  spectool -g -R ${SPEC_FILE}
+  spectool ${SPEC_FILE} | grep -Po '(?<=: )(?!http).+' | xargs -I{} cp {} $(rpm -E '%{_sourcedir}')
+  rpmbuild -bs ${SPEC_FILE} -D "_srcrpmdir ${PWD}"
   dnf builddep -y ./*.src.rpm
   rpmbuild --rebuild ./*.src.rpm -D "_rpmdir ${1}" -D "_srcrpmdir ${1}"
   mv ./*.src.rpm "${1}/source"
