44a1358d6c085eedf721debe8a1d586fbaa8ddba
Dudecake <ckoomen@ckoomen.eu>
2024-02-10 09:46:20 +0100

    Only call `dnf builddep` when spec contains `BuildRequires`

--- build-rpm.sh
+++ build-rpm.sh
@@ -7,7 +7,9 @@ if [[ ! -z "${FORCE_REBUILD}" || ! -f "${2}/source/tree/${RPM_FILE}" ]]; then
   spectool -g -R ${SPEC_FILE}
   spectool ${SPEC_FILE} | grep -Po '(?<=: )(?!http).+' | xargs -I{} cp {} $(rpm -E '%{_sourcedir}')
   rpmbuild -bs ${SPEC_FILE} -D "_srcrpmdir ${PWD}"
-  dnf builddep -y ./*.src.rpm
+  if grep -q 'BuildRequires:' "${SPEC_FILE}"; then
+    dnf builddep -y ./*.src.rpm
+  fi
   rpmbuild --rebuild ./*.src.rpm -D "_rpmdir ${1}" -D "_srcrpmdir ${1}"
   mv ./*.src.rpm "${1}/source"
 else
