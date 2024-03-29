f6f18f934ca3d61e7374df76e36e3c300d8b417b
Dudecake <ckoomen@ckoomen.eu>
2024-03-29 19:15:37 +0100

    Inlined `_metainfodir` macro

--- cosmic/build-rpm.sh
+++ cosmic/build-rpm.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-set -ex
+set -e
 
 script_dir="${PWD}"
 
@@ -21,7 +21,7 @@ for dir in ./*/; do
     sed 's/%autorelease/1/g' ./${_package}.spec > ../../cosmic-rpms/${_package}/${_package}.spec
     . ./srpm.sh
     mkdir ../${_package}
-    sed 's/dbus-devel/dbus-1-devel/g; s/libappstream-glib/libappstream-glib8/g; s/libseat-devel/seatd-devel/g; s/mesa-libgbm-devel/libgbm-devel/g; /rust-rav1e+nasm-rs-devel/d; s/rustc/rust/g' ./${_package}.spec > ../${_package}/${_package}.spec
+    sed 's/dbus-devel/dbus-1-devel/g; s/libappstream-glib/libappstream-glib8/g; s/libseat-devel/seatd-devel/g; s/mesa-libgbm-devel/libgbm-devel/g; /rust-rav1e+nasm-rs-devel/d; s/rustc/rust/g; s/%{_metainfodir}/%{_datadir}\/metainfo/g' ./${_package}.spec > ../${_package}/${_package}.spec
     if [[ -f ./vendor.tar ]]; then
       mv ./${_package}.tar.gz "$(rpm -E '%_sourcedir')"
       mv vendor.tar ../${_package}
