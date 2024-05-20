87745e9ddd2b14176b393ebfef2b95bfcd5c51b4
Dudecake <ckoomen@ckoomen.eu>
2024-05-20 12:24:57 +0200

    Removed `light`

--- light/build-rpm.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-PKG=light
-BRANCH=f38
-set -e
-../fetch-fedora-pkg.sh ${PKG} ${BRANCH}
-sed -i '/%make_install/a rm -rf %{buildroot}%{_datadir}/doc' ./${PKG}.spec
-
-exec ../build-rpm.sh "$@"
