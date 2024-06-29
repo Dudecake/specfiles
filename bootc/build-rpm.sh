f5c2bbbbde80dd6ed3e9e297a0304dd14390bc88
Dudecake <ckoomen@ckoomen.eu>
2024-06-29 10:08:27 +0200

    Optionally override `bootc` version

--- bootc/build-rpm.sh
+++ bootc/build-rpm.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+set -ex
+
 curl -sSLO https://github.com/containers/bootc/raw/main/contrib/packaging/bootc.spec
+
+version="$(grep -Po '(?<=Version:)\s*\d+\.\d+(\.\d+)?' bootc.spec)"
+
+verlte() {
+    [  "$1" = "`echo -e "$1\n$2" | sort -V | head -n1`" ]
+}
+
+verlt() {
+    [ "$1" = "$2" ] && return 1 || verlte $1 $2
+}
+
+verlt "${version}" '0.1.13' && sed -i "s:${version}:0.1.13:g" bootc.spec
 
 exec ../build-rpm.sh "$@"
