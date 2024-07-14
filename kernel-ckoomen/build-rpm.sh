facea32551f5f0d7b3c5bfe6059e65c3b38e1f13
Dudecake <ckoomen@ckoomen.eu>
2024-07-14 11:01:59 +0200

    Disable some kernel params

--- kernel-ckoomen/build-rpm.sh
+++ kernel-ckoomen/build-rpm.sh
@@ -26,6 +26,8 @@ touch "${srcdir}/TOLERATE-UNKNOWN-NEW-CONFIG-OPTIONS"
 if [[ ! -z "${SBOOT_BASE}" ]]; then
   cp ${SBOOT_BASE}.* "${srcdir}"
   cp ${SBOOT_BASE}.crt "${srcdir}/_projectcert.crt"
+  mkdir "${srcdir}/.kernel_signing_certs"
+  cp ${SBOOT_BASE}.crt "${srcdir}/.kernel_signing_certs/"
 fi
 popd > /dev/null
 rpmbuild -bs "${specfile}" -D "_srcrpmdir ${PWD}" -D "_sourcedir ${srcdir}"
@@ -34,7 +36,7 @@ if [[ ! -z "${FORCE_REBUILD}" || ! -f "${2}/source/tree/${RPM_FILE}" ]]; then
   dnf builddep -y ./*.nosrc.rpm
   dnf install -y /usr/bin/openssl
   BRP_PESIGN_COMPRESS_MODULE=${COMPRESS_MODULES} rpmbuild -bb "${specfile}" -D "_rpmdir ${1}" -D "_srcrpmdir ${1}" -D "_sourcedir ${srcdir}"
-  mkdir "${1}/noarch"
+  mkdir -p "${1}/noarch"
   mv ./kernel-ckoomen-*.nosrc.rpm "${1}/noarch"
 else
   echo "No rebuild neccesary for package $(basename $PWD)" >&2
