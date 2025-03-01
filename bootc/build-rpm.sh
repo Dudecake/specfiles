0e95e625a85881e6b1a0af61fc5293983d21b893
Dudecake <ckoomen@ckoomen.eu>
2025-03-01 20:16:38 +0100

    Updated `bootc`

--- bootc/build-rpm.sh
+++ bootc/build-rpm.sh
@@ -1,19 +1,9 @@
 #!/bin/bash
 
-set -ex
-
-curl -sSLO https://github.com/containers/bootc/raw/main/contrib/packaging/bootc.spec
-
-version="$(grep -Po '(?<=Version:)\s*\d+\.\d+(\.\d+)?' bootc.spec)"
-
-verlte() {
-    [  "$1" = "`echo -e "$1\n$2" | sort -V | head -n1`" ]
-}
-
-verlt() {
-    [ "$1" = "$2" ] && return 1 || verlte $1 $2
-}
-
-verlt "${version}" '0.1.13' && sed -i "s:${version}:0.1.13:g" bootc.spec
-
+PKG=bootc
+BRANCH=rawhide
+set -e
+../fetch-fedora-pkg.sh ${PKG} ${BRANCH}
+sed -i '/cargo-rpm-macros/a BuildRequires: cargo-packaging' ${PKG}.spec
+sed -i '/cargo-rpm-macros/d' ${PKG}.spec
 exec ../build-rpm.sh "$@"
